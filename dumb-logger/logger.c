49c40d35c20f594d15f1e063d0d05fb41cffdf1f
zyzo <zyzo@github.com>
2015-04-08 20:48:05 +0200

    update dumb-logger library

--- dumb-logger/logger.c
+++ dumb-logger/logger.c
@@ -1,7 +1,14 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 #include "logger.h"
+
+#define MAX_PREFIX_LEN 20
+
 static int logger_level = LOGGER_SILENT;
+
+static char prefix[MAX_PREFIX_LEN];
+static int require_prefix = 0;
 
 int logger_set_level(int mode) {
 	if (mode < 0 || 1 < mode) {
@@ -12,9 +19,29 @@ int logger_set_level(int mode) {
 	return 0;
 }
 
+int logger_get_level() {
+	return logger_level;
+}
+
+int logger_set_prefix(char* custom_prefix) {
+	if (strlen(custom_prefix) == 0 ||  MAX_PREFIX_LEN < strlen(custom_prefix)) {
+		printf("Prefix invalid. Please check prefix length (maximum %d)\n", MAX_PREFIX_LEN);
+		return -1;
+	}
+	strcpy(prefix, custom_prefix);
+	require_prefix = 1;
+	return 0;
+}
+
 int logger_error(const char* fmt, ...) {
+
 	va_list args;
 	va_start(args, fmt);
+	if (require_prefix) {
+		printf("[%s]", prefix);
+	}
+	printf("[e] ");
+
 	vprintf(fmt, args);
 	va_end(args);
 	return 0;
@@ -26,6 +53,10 @@ int logger_info(const char* fmt, ...) {
 	}
 	va_list args;
 	va_start(args, fmt);
+	if (require_prefix) {
+		printf("[%s]", prefix);
+	}
+	printf("[i] ");
 	vprintf(fmt, args);
 	va_end(args);
 	return 0;
--- dumb-logger/logger.h
+++ dumb-logger/logger.h
@@ -7,3 +7,7 @@ int logger_error(const char* fmt, ...) ;
 int logger_info(const char* fmt, ...);
 
 int logger_set_level(int mode);
+
+int logger_get_level();
+
+int logger_set_prefix(char* custom_prefix);
