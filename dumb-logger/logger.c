fc53221e414f5fb20d87aed3da406a712fb32fae
antho31 <anthonygourraud@gmail.com>
2015-05-07 12:05:41 +0200

    affichage ligne C pour erreurs

--- dumb-logger/logger.c
+++ dumb-logger/logger.c
@@ -6,9 +6,15 @@
 #define MAX_PREFIX_LEN 20
 
 static int logger_level = LOGGER_SILENT;
-
-static char prefix[MAX_PREFIX_LEN];
 static int require_prefix = 0;
+static char prefix[MAX_PREFIX_LEN];
+
+int nb_error_occured = 0 ;  
+char* nom_fichier_C ; 
+
+void logger_set_nom_fichier(char* name){
+  nom_fichier_C = name ; 
+} 
 
 int logger_set_level(int mode) {
 	if (mode < 0 || 1 < mode) {
@@ -23,6 +29,11 @@ int logger_get_level() {
 	return logger_level;
 }
 
+int get_nb_errors_occured() {
+	return nb_error_occured;
+}
+
+
 int logger_set_prefix(char* custom_prefix) {
 	if (strlen(custom_prefix) == 0 ||  MAX_PREFIX_LEN < strlen(custom_prefix)) {
 		printf("Prefix invalid. Please check prefix length (maximum %d)\n", MAX_PREFIX_LEN);
@@ -33,19 +44,36 @@ int logger_set_prefix(char* custom_prefix) {
 	return 0;
 }
 
+/* affichage d'une erreur san infos sur le fichier, ni la ligne */ 
 int logger_error(const char* fmt, ...) {
-
+        nb_error_occured++ ;
 	va_list args;
 	va_start(args, fmt);
 	if (require_prefix) {
 		printf("[%s]", prefix);
 	}
 	printf("[e] ");
-
+ 
 	vprintf(fmt, args);
 	va_end(args);
 	return 0;
 }
+
+/* affichage d'une erreur avec infos sur le fichier et la ligne */ 
+int logger_lerror(int lino, const char* fmt, ...) {
+        nb_error_occured++ ;
+	va_list args;
+	va_start(args, fmt);
+	if (require_prefix) {
+		printf("[%s]", prefix);
+	}
+	printf("[e] [%s:%d] ",nom_fichier_C, lino);
+ 
+	vprintf(fmt, args);
+	va_end(args);
+	return 0;
+}
+
 
 int logger_info(const char* fmt, ...) {
 	if (logger_level != LOGGER_VERBOSE) {
--- dumb-logger/logger.h
+++ dumb-logger/logger.h
@@ -2,12 +2,18 @@
 #define LOGGER_SILENT   0
 #define LOGGER_VERBOSE  1
 
+void logger_set_nom_fichier(char* name) ;
+
 int logger_error(const char* fmt, ...) ;
+
+int logger_lerror(int lino, const char* fmt, ...) ;
 
 int logger_info(const char* fmt, ...);
 
 int logger_set_level(int mode);
 
 int logger_get_level();
+
+int get_nb_errors_occured() ;
 
 int logger_set_prefix(char* custom_prefix);
