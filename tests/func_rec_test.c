cea2d5c22cefda276986902c1726d4383a955fb1
antho31 <anthonygourraud@gmail.com>
2015-05-08 15:21:21 +0200

    MAJ fichiers tests erreurs detectees

--- tests/func_rec_test.c
+++ tests/func_rec_test.c
@@ -1,11 +1,13 @@
-int a = 5;
-
+/*int a = 1 + 2 * 2 + - 1 ;
+int b = a/2 ; */
+int a = -3 ;  
 p() {
 	printf(a);
 	if (a) {
-		a = a - 1;
+		a = a + 1;
 		p();
 	}
+printf(a) ; 
 }
 
 main() {
--- tests/test_toutes_fonctionnalites.c
+++ tests/test_toutes_fonctionnalites.c
@@ -6,21 +6,45 @@ int variable = b  ;
 ok3(int a) ; ok4() ; 
 
 // définition de la fonction ok(const int b) 
-ok(const int b2, int c, int d){
-  int a = 1 ; 
+ok(const int b2, int c, int d){   
+  printf("fonction ok") ; 
   variable = 77 ; 
+  printf(4+2) ; 
 }
 
 ok5() ; 
 
 ok2(){ 
-  int z = 14 ;  
-  ok(z,z,z) ; ok3(5+z/(b+b)) ; 
+  int z = 14 ; 
+  int cpt = 4 ;  
+
+  printf("fonction ok2") ; 
+  
+  printf("cpt =") ; 
+  printf(cpt) ;
+
+  ok(5,7,2) ; 
+
+  printf("cpt =") ; 
+  printf(cpt) ;
+
+  ok3(5+z/(b+b)) ; 
+
+  printf("cpt =") ; 
+  printf(cpt) ;
+
+  printf(variable) ; 
+
   if (b/7) { 
     z = 7 ; 
   }   
-  while(1) {
-    printf(z+variable*7) ; 
+  printf("cpt =") ; 
+  printf(cpt) ;
+
+  while(0) {
+    variable = -cpt * 5 + 2 / b ; 
+    cpt = cpt - 1 ; 
+    printf(cpt) ; 
   }
 } 
 
@@ -28,12 +52,14 @@ ok2(){
 main() { 
   const int c1,c2,c3 = 1 ; 
   int a = 1, j, i = 7, z = 12;  
-  int test1 = 3 *  -b / 2 ; 
-  test1 = b - 2 ; 
+  int test1 = 3 *  b / 2 + 15/5 ; // 9 
+  printf(test1) ; 
+  test1 = b - 2 + -7 * 2 ; // -12 
+  printf(test1) ; 
+  variable = ( z + a ) + 2 * -5 +  1  ; // égal à 4
   printf(variable) ; 
-  //variable = ( z + a ) + 2 * -5 +  1  ; // égal à 4
-  printf(variable) ; 
-  //ok2() ;   ok((z*b*2+9-a),4/3+c3,111)  ;
+  ok2() ;   ok((z*b*2+9-a),4/3+c3,111)  ;
+  variable = 2 ; printf(variable) ; 
   if(variable) { 
    a = 444; 
   } 
@@ -50,8 +76,10 @@ ok3(int a){
 
 // définition de la fonction ok3(int a) précédemment déclarée
 ok3(int a){
-  int z = 44 ;  
+  int z = 4 ;  
+  printf("fonction ok3") ; 
   while(z) {
-  printf("test") ;
+  z = z - 1 ; 
+  printf(z-1) ;
   } 
 } 
